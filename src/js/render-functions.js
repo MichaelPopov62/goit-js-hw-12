f0a420bb31dbbbff2df21b32c5242f094f66ef88
MichaelPopov62 <romantikzizni@gmail.com>
2025-05-24 16:34:41 +0300

    add changed js html css

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,17 +1,17 @@
-/*createGallery(images) — формує і додає HTML в .gallery.
-
-clearGallery() — очищає HTML в .gallery.
-
-showLoader() / hideLoader() — керують індикатором завантаження.
-
-showLoadMoreButton() / hideLoadMoreButton() — показують або ховають кнопку завантаження.*/
 
 
-// Імпортую бібліотеку для лайтбоксу
+/* Які діі в цьому файлі:
+1.createGallery(images) — формує і додає HTML в .gallery.
+2.clearGallery() — очищає HTML в .gallery.
+3.showLoader() / hideLoader() — керують індикатором завантаження.
+4.showLoadMoreButton() / hideLoadMoreButton() — показують або ховають кнопку завантаження.*/
+
+
+/* Імпортую бібліотеку для лайтбоксу.ДЛя перегляду зображень в модальному вікні. Бібліотека SimpleLightbox надає зручний перегляд зображень у вигляді модального вікна зі збільшенням.*/
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// Знаходжу контейнер для галереї
+// Знаходжу контейнер для галереї. Щоб надалі працювати з елементами,зберігаю їх у змінних.
 const galleryContainer = document.querySelector('.gallery'); // Вибираю елемент галереї за класом "gallery"
 
 // знаходжу кнопку для пролістування галереі
@@ -23,10 +23,14 @@ const loader = document.querySelector('.loader');
 // перевірка елемента галереї
 // console.log('Контейнер галереї знайдено:', galleryContainer);
 
-let lightbox = null; // Змінна для екземпляра SimpleLightbox
+let lightbox = null; // Змінна для екземпляра SimpleLightbox.Вона буде містить екземпляр бібліотеки SimpleLightbox
 // console.log('Ініціалізовано змінну для SimpleLightbox:', lightbox); // перевіряю початкового значення змінної
 
-// Функція для створення галереї з масиву зображень
+/* Функція для створення галереї з масиву зображень, зображення будут рендеріться.
+ 1. Перетворюємо кожне зображення з масиву на шматок HTML-коду, щоб відобразити його в галереї.
+ 2.Усі згенеровані HTML-елементи вставляються всередину контейнера з класом .gallery, тобто на сторінку.
+ 3. Якщо модальне вікно для перегляду зображень (lightbox) уже створене — оновлюємо його, щоб показував нові фото. Якщо його ще немає — створюємо нове.
+ 4.Якщо в галереї вже є хоча б одне зображення — показуємо кнопку "Load more", щоб користувач міг завантажити ще.*/
 export function createGallery(images) {
   // Перевіряю отриманий масив зображень
   console.log('Виклик createGallery. Масив зображень:', images);
@@ -96,7 +100,9 @@ export function createGallery(images) {
     throw error;
   }
 }
-// Функція для очищення галереї
+
+/* Функція для очищення галереї, приховування кнопки "Loadmore".
+Видаля. весь вміст галереї перед новим запитом. Також ховаю кнопку "Load more", бо вона не потрібна під час нового пошуку.*/
 export async function clearGallery() {
   try {
     galleryContainer.innerHTML = ''; // видаляю весь HTML-контент с контейнера галереї
@@ -113,15 +119,14 @@ export async function clearGallery() {
   }
 }
 
-// Функція для відображення
+// Функціі для того щоб під час запиту показую лоадер, після завершення — ховаю.
 
-export async function showLoader() {
+export async function showLoader() { //керує коли треба показувати лоадер
   // console.log('Виклик функції showLoader.'); //  виклику функції
 
   try {
     //
-    loader.style.display = 'block';
-
+    loader.style.display = 'block'; /*Тут я показую що індикатор завантаження (loader) на сторінці.Перед тим, як починаєтся завантажуваня зображення з API, я викликаю цю команду, щоб зрозуміти-йде завантаження.Потім коли завантаження завершується я ховаю лоадер іншою функцією*/
     console.log('Клас "loading" додано до тега <body>.'); // підтвердження додавання класу після невірного вводу
   } catch (error) {
     // Обробка помилок під час відображення лоадера
@@ -134,8 +139,7 @@ export async function showLoader() {
 export async function hideLoader() {
   // console.log('Виклик функції hideLoader.'); //  виклик
   try {
-    //  document.body.classList.remove('loading');  // Видаляю клас "loading" з <body>, щоб приховати стилі лоадера
-    loader.style.display = 'none';
+    loader.style.display = 'none'; //Коли завантаження зображень завершено, я використовую цей рядок, щоб сховати спінер , який показує, що щось завантажується. Елемент буде ніби видалений зі сторінки (але фізично залишиться в коді)
     // console.log('Клас "loading" видалено з тега <body>.'); //  підтвердження видалення класу
   } catch (error) {
     // Обробка помилок під час приховування лоадера
@@ -143,10 +147,12 @@ export async function hideLoader() {
     throw new Error('Не вдалося приховати лоадер.');
   }
 }
-// Показати кнопку
+
+/* Кнопка "Load more" зʼявляється лише якщо є що завантажити. Коли починаєтьсчя новий пошук — ховаю її. Після успішного завантаження — показую знову.Для куруванням кнопкою додаю дві функціі*/
+//показую кнопку
 export async function showLoadMoreButton() {
   try {
-    loadMoreButton.classList.remove('is-hidden');
+    loadMoreButton.classList.remove('is-hidden'); //прибираю клас is-hidden який раніше ховав цю кнопку.Виконується тоді коли зображень більш ніж одна сторінка
   } catch (error) {
     console.error('Помилка при показі кнопки Load more:', error);
     throw error;
@@ -155,7 +161,7 @@ export async function showLoadMoreButton() {
 // Приховати кнопку
 export async function hideLoadMoreButton() {
   try {
-    loadMoreButton.classList.add('is-hidden');
+    loadMoreButton.classList.add('is-hidden'); //додаю клас is-hidden який раніше був знятий.Виконується тоді коли зображень тільки на одну сторінку
   } catch (error) {
     console.error('Помилка при приховуванні кнопки Load more:', error);
     throw error;
--- src/main.js
+++ src/main.js
@@ -1,104 +1,116 @@
 
-/*
+ /*Що робиться в файлі.
   При сабміті форми (submit):
-  1. Очищується галерея (щоб не змішувати старі та нові результати) і скидається сторінка (currentPage = 1), адже починаю пошук заново з першої сторінки.
+  1. Очищується галерея (щоб не змішувати старі та нові результати) і скидається сторінка (currentPage = 1), одже починаю пошук заново з першої сторінки.
   2. Запитую першу сторінку результатів за новим пошуковим запитом.
   3. Показую повідомлення із кількістю знайдених зображень, щоб користувач бачив результат.
   4. Кнопка Load More робиться видимою, якщо є ще сторінки для завантаження.
 
-  При кліку на Load More:
+ При кліку на Load More:
   1. Запитую наступну сторінку (підвищую currentPage після кожного запиту).
   2. Додаю нові зображення до галереї, не видаляючи попередні.
   3. Оновлюю SimpleLightbox (refresh) — щоб нові зображення також відкривались у лайтбоксі.
   4. Якщо досягнуто останньої сторінки — ховаю кнопку і показую повідомлення.
   5. Прокрутка сторінки виконується так, щоб користувач бачив новий контент (плавний скрол вниз).
-*/
+ */
 
 
-//імпортую функціі і бібліотеку
+// //імпортую функціі і бібліотеку
 import { getImagesByQuery } from './js/pixabay-api.js';
+/*перевірка на роботу скріпта
+console.log('Скрипт main.js працює');
+
+getImagesByQuery('test').then(data => {
+  console.log('Отримано дані з Pixabay:', data);
+});*/
+
 import {
   createGallery,
   clearGallery,
   showLoadMoreButton,
   hideLoadMoreButton,
-} from './js/render-functions.js';
-import iziToast from 'izitoast';// бібліотека для яскравих повідомлень користувачу
-import 'izitoast/dist/css/iziToast.min.css';
+} from './js/render-functions.js';  //функціі для візуального оновленя DOM
+import iziToast from 'izitoast';    // бібліотека для яскравих повідомлень користувачу
+import 'izitoast/dist/css/iziToast.min.css';  // стилі для iziToast
 
-//шукаю елементи
-const form = document.querySelector('.form');//форма пошуку
+//шукаю елементи, отримую DOM
+const form = document.querySelector('.form');                  //форма пошуку
 const input = form.querySelector('input[name="search-text"]');//поле виведення пошукового текста
-const loader = document.querySelector('.loader');// індікатор завантаження
-const loadMoreButton = document.querySelector('.select');// кнопка (load more)
-const gallery = document.querySelector('.gallery');// контейнер галереї
+const loader = document.querySelector('.loader');            // індікатор завантаження
+const loadMoreButton = document.querySelector('.select');   // кнопка (load more)
+const gallery = document.querySelector('.gallery');        // контейнер галереї
 
 //Змінні для курування станом пошуку
-let currentQuery = '';// поточний пошуковий запит
-let currentPage = 1;// номер поточної сторінки пошукового запиту
-let totalHits = 0;// загальна кількість знайдених зображень
-let isLoading = false;// прапарець щоб уникнути дублювання запитів
+let currentQuery = '';     // поточний пошуковий запит
+let currentPage = 1;      // номер поточної сторінки пошукового запиту
+let totalHits = 0;       // загальна кількість знайдених зображень
+let isLoading = false;  // прапарець щоб уникнути повторного запиту коли один вже триває
 
 // функція для показу індікатора завантаження
 function showLoader() {
   if (loader) loader.style.display = 'block'; //style.display керує видимістю елемента в DOM через CSS.'block' — робить елемент видимим і він займає окремий блок (повний рядок по ширині).//
+  console.log('Loader показано: видимість встановлена на "block".'); // Лог для перевірки
 }
 
-//функція для сховування індікатора завантаження
+//функція для сховування індікатора завантаження.Для того щоб бачити що шось завантажується
 function hideLoader() {
-  if (loader) loader.style.display = 'none';
+  if (loader) loader.style.display = 'none'; //робить схованим лоадер
+
+  console.log('Loader сховано: видимість встановлена на "none".'); // Лог для перевірки
 }
 
-//функція для симуляціі завантаження, чекати завантаження
+//функція для симуляціі завантаження, чекати завантаження. Я зробив для того щоб при швидкому інтернеті бачити процес завантаження
 async function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 /* Основна функція для завантаження зображень
  isFirstLoad = true - перший запит (початковий пошук),
-false - запит при натисканні Load More*/
+false - запит при натисканні Load More. Тобто для блокування повторного виклику*/
 async function loadImages(isFirstLoad = false) {
-  if (isLoading) return;
+  if (isLoading) {
+    console.log('Запит уже виконується, новий запит не запускається.');
+    return;
+  }
 
   isLoading = true;
+  console.log('Початок завантаження зображень.'); // Лог на початок завантаження
+
   showLoader(); // показую індікатор що данні завантажені
   hideLoadMoreButton(); // ховаю кнопку поки не знаю, чи є ще сторінки
 
   try {
-    await delay(1000); // штучна затримка виконання коду ( для тесту )функціі loadImages
+    await delay(2000); // штучна затримка виконання коду ( для тесту )функціі loadImages
 
     // роблю запит до Pixabay за поточним запитом і сторінкою
     const data = await getImagesByQuery(currentQuery, currentPage);
 
     // Якщо немає результатів — очищаємо галерею і повідомляємо користувача
     if (!data.hits || data.hits.length === 0) {
-      clearGallery();
-      console.clear();
-      iziToast.info({
+      clearGallery(); // очищаю галерею
+      iziToast.info({ //Виводжу повідомлення
         title: 'Нічого не знайдено',
         message:
           'Sorry, there are no images matching your search query. Please try again!',
         position: 'topRight',
       });
-      hideLoadMoreButton();
-      hideLoader();
+      hideLoadMoreButton(); //ховаю кнопку 
+      hideLoader();        //ховаю індікатор завантаження
+      // console.log('Завершення завантаження: результатів немає.');
       isLoading = false;
       return; // припиняю виконання, бо нема що показувати
     }
 
     // Якщо це перший запит, очищую старі результати та оновлюю загальну кількість
     if (isFirstLoad) {
-      clearGallery();
+      clearGallery(); // видаляю старі данні(зображення)
       totalHits = data.totalHits; // оновлюю загальну кількість знайдених зображень
 
       // Обчислюємо максимальну кількість сторінок (по 15 зображень на сторінку)
       const maxPages = Math.ceil(totalHits / 15);
-      console.clear();
-      console.log(`Новий запит: "${currentQuery}"`);
-      console.log(
-        `Загальна кількість результатів: ${totalHits}, Максимальна кількість сторінок: ${maxPages}`
-      );
 
+      // console.log(`Новий запит: "${currentQuery}"`);// показую скільки сторінок
+      
       // Повідомлення користувачу, що пошук успішний
       iziToast.success({
         title: 'Успіх',
@@ -107,9 +119,11 @@ async function loadImages(isFirstLoad = false) {
         timeout: 2000,
       });
     }
+
     // Створюю в DOM елементи для отриманих зображень і додаємо їх до галереї
     createGallery(data.hits);
 
+    //додаю скрол.Зручно бачити що новий контент додано 
     if (isFirstLoad) {
       // При першому завантаженні скролю сторінку вверх, щоб бачити початок галереї
       window.scrollTo({
@@ -140,32 +154,32 @@ async function loadImages(isFirstLoad = false) {
     console.log(`Поточна сторінка: ${currentPage} / ${maxPages}`);
 
     if (currentPage >= maxPages) {
-      // Якщо це остання сторінка — ховаю кнопку і виводжу повідомлення
+      // Якщо це остання сторінка — виводжу повідомлення
       iziToast.info({
         title: 'Кінець колекції',
         message: "We're sorry, but you've reached the end of search results.",
         position: 'topRight',
       });
-      hideLoadMoreButton();
+      hideLoadMoreButton(); //ховаю кнопку
     } else {
-      // Якщо сторінки залишились — збільшую currentPage та показую кнопку
-      currentPage += 1;
-      showLoadMoreButton();
-    }
-  } catch (error) {
 
+      // Якщо сторінки залишились — збільшую currentPage
+      currentPage += 1;
+      showLoadMoreButton();// показую кнопку для наступноі сторінки
+    }
+    //завершеня функціі
+  } catch (error) {
     // Якщо сталася помилка при запиті — показую повідомлення і лог в консоль
     console.error('Помилка запиту:', error);
-    iziToast.error({
+    iziToast.error({  //повідомлення про помилку
       title: 'Помилка',
       message: 'Не вдалося завантажити зображення. Спробуйте пізніше.',
       position: 'topRight',
     });
   } finally {
-
-    // В будь-якому випадку — ховаю індикатор і скидаю прапорець завантаження
-    hideLoader();
-    isLoading = false;
+    // В будь-якому випадку 
+    hideLoader(); //ховаю індикатор
+    isLoading = false; // скидаю прапорець завантаження
   }
 }
 
@@ -178,7 +192,7 @@ form.addEventListener('submit', async event => {
 
   // Якщо користувач не ввів запит — показую помилку і виходжу
   if (!query) {
-    iziToast.error({
+    iziToast.error({ // повідомлення якщо порожній запит
       title: 'Помилка',
       message: 'Будь ласка, введіть пошуковий запит.',
       position: 'topRight',
@@ -191,8 +205,11 @@ form.addEventListener('submit', async event => {
   currentPage = 1;
   totalHits = 0;
 
+  // Очищаю галерею перед новим запитом
+  clearGallery();
+
   // Завантажую першу сторінку з новим запитом
-  await loadImages(true); // Очікую виконання запиту та оновлення галереї
+  await loadImages(true); // Очікую виконання запиту та оновлення .Тобто новий запит
 
   // Очищаю поле вводу, щоб було зручно робити нові пошуки
   input.value = '';
@@ -205,6 +222,7 @@ loadMoreButton.addEventListener('click', async () => {
   // Завантажую наступну сторінку (isFirstLoad = false)
   await loadImages(false); // Очікую доки завантажаться наступні зображення
 });
+
 
 
 
